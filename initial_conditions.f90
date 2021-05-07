3430c69aa769885e6c3207d2bdcf532ac627f9cc
axl-art <axeljamilarteaga26@gmail.com>
2021-05-07 17:18:08 -0500

    .

--- initial_conditions.f90
+++ initial_conditions.f90
@@ -1,12 +1,13 @@
 PROGRAM initial-conditions
 
-    INTEGER CI(900,900)
-    
-    !OPEN FILE
-    open(1, File='CI.dat')
+    INTEGER, PARAMETER :: N = 900
+    INTEGER, DIMENSION (N,N) :: CI
 
-    DO i = 1, 900
-        DO j = 1, 900
+    !OPEN FILE
+    OPEN(1, File='CI.dat')
+
+    DO i = 1, N
+        DO j = 1, N
             CI(i,j) = 0
         END DO      
     END DO
@@ -20,8 +21,8 @@ PROGRAM initial-conditions
     CI(450,450) = 1
 
     !EXIT
-    DO j = 1, 900
-        WRITE(*,*) (CI(i,j) , i = 1, 900)
+    DO j = 1, N
+        WRITE(*,*) (CI(i,j) , i = 1, N)
     END DO
 
 END PROGRAM
